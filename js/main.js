44fb8d4f51b3d65df427fcb75d6583d7f516e35a
Evgenia99-web <proximastar@yandex.ru>
2025-05-18 12:49:11 +0300

    add 2 pages

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView:2,
+  
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  AOS.init();
