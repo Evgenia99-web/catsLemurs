77d986d2383b35c8c97b95257d4ff8e9adcde176
Evgenia99-web <proximastar@yandex.ru>
2025-05-21 13:27:33 +0300

    update code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
-    slidesPerView:2,
+    slidesPerView:1,
   
 
     // Navigation arrows
@@ -9,6 +9,17 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  });
+});
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    var btn = document.querySelector('.btn_show > span')
+    if(e.style.display == 'block'){
+        e.style.display = 'none';
+        btn.innerHTML = 'Скрыть скелет';}
+    else{
+       e.style.display = 'block';
+       btn.innerHTML = 'Показать скелет';}
+}
 
 AOS.init();
